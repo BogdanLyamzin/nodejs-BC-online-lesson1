146b1f483ca659ac4cfe8fd305b2cdd5fb0b91e6
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-02 19:23:44 +0200

    add new lesson

---
+++ node-module-es6/index.js
@@ -0,0 +1,21 @@
+const users = require("./users");
+console.log(users);
+// console.log(admins);
+// console.log(managers);
+
+// const currentMonth = require("./currentMonth");
+// console.log(currentMonth)
+
+// const currentDate = require("./currentDate");
+// console.log(currentDate)
+
+// const currentMonthNumber = require("./getMonth")();
+// console.log(`Номер текущего месяца - ${currentMonthNumber}`)
+
+// const [firstPeople, ...rest] = require("./people");
+// console.log(firstPeople);
+// console.log(rest);
+
+const {form, modal} = require("./views");
+console.log(form);
+console.log(modal);
